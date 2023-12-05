c7e66e482a722d82e10af7bc1daae8e9eccfe202
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-12-05 17:25:46 +0100

    CRUD for product page

--- orders-dashboard-frontend/src/app/pages/products/products.component.ts
+++ orders-dashboard-frontend/src/app/pages/products/products.component.ts
@@ -1,28 +1,47 @@
-import {Component, inject, OnInit, TemplateRef} from '@angular/core';
-import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {Component, inject, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {NgbActiveModal, NgbModal, NgbToast} from "@ng-bootstrap/ng-bootstrap";
 import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {ProductsServiceService} from "../../shared/services/product/products-service.service";
 import {ProductDto} from "../../shared/domain/product-dto";
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [
     NavbarComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CurrencyPipe,
+    NgbToast
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent implements OnInit {
-  productForm = new FormGroup({
+  productFormCreate = new FormGroup({
     name: new FormControl('', [Validators.required]),
-    price: new FormControl('', [Validators.required, Validators.pattern("\\d+")]),
+    price: new FormControl('', [Validators.required, Validators.pattern("\\d+\.?\\d{0,2}")]),
     quantity: new FormControl('', [Validators.required, Validators.pattern("\\d+")])
   });
+
+  productFormUpdate = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required, Validators.pattern("\\d+\.?\\d{0,2}")]),
+    quantity: new FormControl('', [Validators.required, Validators.pattern("\\d+")])
+  });
+
   products: ProductDto[] = [];
+
+  @ViewChild('update') updateModal!: TemplateRef<any>;
+
+  @ViewChild('deleteConfirmation') deleteModal!: TemplateRef<any>;
+
+  public deleteMsg: string = "";
+
   private modalService = inject(NgbModal);
+
+  private selectedProduct?: ProductDto;
 
   constructor(private productsService: ProductsServiceService) {
   }
@@ -38,9 +57,9 @@ export class ProductsComponent implements OnInit {
   onCreateSubmit(modal: NgbActiveModal) {
     console.log("onCreateSubmit");
     const product: ProductDto = {
-      name: this.productForm.value.name!,
-      price: Number.parseInt(this.productForm.value.price!),
-      availability: Number.parseInt(this.productForm.value.quantity!)
+      name: this.productFormCreate.value.name!,
+      price: Number.parseFloat(this.productFormCreate.value.price!),
+      availability: Number.parseInt(this.productFormCreate.value.quantity!)
     };
 
     this.productsService.createProduct(product).subscribe((data) => {
@@ -48,16 +67,56 @@ export class ProductsComponent implements OnInit {
     });
 
     modal.close();
-    this.productForm.reset();
+    this.productFormCreate.reset();
   }
 
   onDelete(id?: number) {
-    console.log(id);
+    if (id == null) return;
+    this.selectedProduct = this.products.find((product) => product.id === id);
+    this.deleteMsg = "Are you sure you want to delete " + this.selectedProduct!.name + "?";
+    this.open(this.deleteModal);
   }
 
-  onEdit() {
-    console.log("onEdit");
+  onDeleteSubmit(modal: NgbActiveModal) {
+    console.log("onDeleteSubmit");
+    this.productsService.deleteProduct(this.selectedProduct?.id!).subscribe((data) => {
+      this.fetchProducts();
+    });
 
+    modal.close();
+  }
+
+  onEdit(id?: number) {
+    if (id == null) return;
+
+    this.open(this.updateModal);
+    this.productsService.getProduct(id).subscribe((data) => {
+      this.selectedProduct = data;
+      this.productFormUpdate.setValue({
+        name: data.name,
+        price: data.price.toString(),
+        quantity: data.availability.toString()
+      });
+    });
+  }
+
+  onEditSubmit(modal: NgbActiveModal) {
+    console.log(this.selectedProduct);
+    const product: ProductDto = {
+      id: this.selectedProduct?.id,
+      name: this.productFormUpdate.value.name!,
+      price: Number.parseFloat(this.productFormUpdate.value.price!),
+      availability: Number.parseInt(this.productFormUpdate.value.quantity!)
+    };
+
+    this.productsService.updateProduct(product).subscribe((data) => {
+      this.fetchProducts();
+    });
+    modal.close();
+  }
+
+  selectAll() {
+    console.log("selectAll");
   }
 
   fetchProducts() {
--- orders-dashboard-frontend/src/app/shared/services/product/products-service.service.ts
+++ orders-dashboard-frontend/src/app/shared/services/product/products-service.service.ts
@@ -15,7 +15,19 @@ export class ProductsServiceService {
     return this.http.post<ProductDto>(this.productsEnpoint, product);
   }
 
+  updateProduct(product: ProductDto) {
+    return this.http.patch<ProductDto>(this.productsEnpoint, product);
+  }
+
   getProducts() {
     return this.http.get<ProductDto[]>(this.productsEnpoint);
+  }
+
+  getProduct(id: number) {
+    return this.http.get<ProductDto>(this.productsEnpoint + "/" + id);
+  }
+
+  deleteProduct(id: number) {
+    return this.http.delete<ProductDto>(this.productsEnpoint + "/" + id);
   }
 }
